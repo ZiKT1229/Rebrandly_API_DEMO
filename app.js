4b7a97e4036b5238bc565be5a2d25a1817987344
KT_Zheng <a6163508@gmail.com>
2019-03-22 12:51:20 +0800

    使用fetch替代XMLHttpRequest

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let API_KEY = ''; // 請輸入自己申請的APIKEY
+let API_KEY = '2dc296eec3a94485a276fdd83b2d3922'; // 請輸入自己申請的APIKEY
 const url = 'https://api.rebrandly.com/v1/links';
 
 const input = document.getElementById("inputField");
@@ -16,19 +16,24 @@ const renderResponse = (res) => {
 const shortenUrl = () => {
   const urlToShorten = inputField.value;
   const data = JSON.stringify({destination: urlToShorten});
-  const xhr = new XMLHttpRequest();
-
-  xhr.responseType = 'json';
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-  		renderResponse(xhr.response)
-		}
-  };
   
-  xhr.open('POST', url);
-  xhr.setRequestHeader('Content-type','application/json');
-  xhr.setRequestHeader('apikey', API_KEY);
-  xhr.send(data);
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'apikey': API_KEY
+    },
+    body: data
+  }).then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+    throw new Error('Request failed!');
+  },networkError => {
+    console.log(networkError.message);
+  }).then(jsonResponse => {
+    renderResponse(jsonResponse);
+  });
 };
 
 button.addEventListener('click', (event) => {
